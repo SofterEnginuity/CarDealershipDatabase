462765a548a415e03f96d1f389e817cc9e044422
Marqae Morris <mamorris920@gmail.com>
2025-06-10 14:45:09 -0400

    database updates in sql

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,9 +1,35 @@
 package com.pluralsight;
 
+import java.sql.*;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
         System.out.println("Hello, World!");
 
-        
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        Connection connection;
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/cardealership", "root", "P@ssw0rd");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        Statement statement = connection.createStatement();
+
+        String query = """
+        SELECT ProductID, ProductName, UnitPrice, UnitsInStock  FROM northwind.products""";
+        ResultSet results = statement.executeQuery(query);
+        while (results.next()){
+            System.out.println("Printing things");
+//            String productName = results.getString("ProductName");
+//            System.out.println("ProductID: " + productID);
+
+        }
+
+
+
+        connection.close();
+
     }
 }
